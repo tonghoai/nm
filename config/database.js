7fae2e6600e617114ecc2cb31ac17df1f0a2acb9
tonghoai <ti.sohotmelody@live.com>
2017-06-15 09:57:29 +0700

    fix for run online

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url' : 'mongodb://localhost/chat' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://hoai:because@ds119682.mlab.com:19682/chat' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
